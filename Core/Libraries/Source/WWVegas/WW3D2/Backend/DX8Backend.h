b0f739c184dec211c375322913b330767f4fb485
Bobby Battista <bobtista@gmail.com>
2026-07-19 13:56:20 +0400

    refactor(ww3d2): add override specifiers to DX8Backend declarations

--- Core/Libraries/Source/WWVegas/WW3D2/Backend/DX8Backend.h
+++ Core/Libraries/Source/WWVegas/WW3D2/Backend/DX8Backend.h
@@ -28,76 +28,76 @@ class DX8Backend : public IRenderBackend
 {
 public:
     DX8Backend();
-    virtual ~DX8Backend();
+    virtual ~DX8Backend() override;
 
-    virtual bool Is_Device_Lost() const;
-    virtual bool Has_Stencil();
-    virtual WW3DFormat Get_Back_Buffer_Format();
-    virtual SurfaceClass * Get_Back_Buffer(unsigned int num);
-    virtual void Set_Gamma(float gamma, float bright, float contrast, bool calibrate, bool uselimit);
+    virtual bool Is_Device_Lost() const override;
+    virtual bool Has_Stencil() override;
+    virtual WW3DFormat Get_Back_Buffer_Format() override;
+    virtual SurfaceClass * Get_Back_Buffer(unsigned int num) override;
+    virtual void Set_Gamma(float gamma, float bright, float contrast, bool calibrate, bool uselimit) override;
 
-    virtual void Begin_Scene();
-    virtual void End_Scene(bool flip_frame);
-    virtual void Flip_To_Primary();
+    virtual void Begin_Scene() override;
+    virtual void End_Scene(bool flip_frame) override;
+    virtual void Flip_To_Primary() override;
     virtual void Clear(bool clear_color, bool clear_z_stencil,
                        const Vector3 & color,
-                       float dest_alpha, float z, unsigned int stencil);
-    virtual void Set_Viewport(const RenderBackendViewport & viewport);
+                       float dest_alpha, float z, unsigned int stencil) override;
+    virtual void Set_Viewport(const RenderBackendViewport & viewport) override;
 
-    virtual void Set_Vertex_Buffer(const VertexBufferClass * vb, unsigned int stream);
-    virtual void Set_Vertex_Buffer(const DynamicVBAccessClass & vba);
-    virtual void Set_Index_Buffer(const IndexBufferClass * ib, unsigned short index_base_offset);
-    virtual void Set_Index_Buffer(const DynamicIBAccessClass & iba, unsigned short index_base_offset);
-    virtual void Set_Index_Buffer_Index_Offset(unsigned int offset);
+    virtual void Set_Vertex_Buffer(const VertexBufferClass * vb, unsigned int stream) override;
+    virtual void Set_Vertex_Buffer(const DynamicVBAccessClass & vba) override;
+    virtual void Set_Index_Buffer(const IndexBufferClass * ib, unsigned short index_base_offset) override;
+    virtual void Set_Index_Buffer(const DynamicIBAccessClass & iba, unsigned short index_base_offset) override;
+    virtual void Set_Index_Buffer_Index_Offset(unsigned int offset) override;
 
-    virtual void Set_Shader(const ShaderClass & shader);
-    virtual void Get_Shader(ShaderClass & shader);
-    virtual void Set_Material(const VertexMaterialClass * material);
-    virtual void Set_Texture(unsigned int stage, TextureBaseClass * texture);
-    virtual void Apply_Render_State_Changes();
-    virtual void Apply_Default_State();
-    virtual void Invalidate_Cached_Render_States();
+    virtual void Set_Shader(const ShaderClass & shader) override;
+    virtual void Get_Shader(ShaderClass & shader) override;
+    virtual void Set_Material(const VertexMaterialClass * material) override;
+    virtual void Set_Texture(unsigned int stage, TextureBaseClass * texture) override;
+    virtual void Apply_Render_State_Changes() override;
+    virtual void Apply_Default_State() override;
+    virtual void Invalidate_Cached_Render_States() override;
 
-    virtual void Set_Transform(TransformKind transform, const Matrix4x4 & m);
-    virtual void Set_Transform(TransformKind transform, const Matrix3D & m);
-    virtual void Get_Transform(TransformKind transform, Matrix4x4 & m);
-    virtual void Set_World_Identity();
-    virtual void Set_View_Identity();
-    virtual bool Is_World_Identity();
-    virtual bool Is_View_Identity();
-    virtual void Set_Projection_Transform_With_Z_Bias(const Matrix4x4 & matrix, float znear, float zfar);
+    virtual void Set_Transform(TransformKind transform, const Matrix4x4 & m) override;
+    virtual void Set_Transform(TransformKind transform, const Matrix3D & m) override;
+    virtual void Get_Transform(TransformKind transform, Matrix4x4 & m) override;
+    virtual void Set_World_Identity() override;
+    virtual void Set_View_Identity() override;
+    virtual bool Is_World_Identity() override;
+    virtual bool Is_View_Identity() override;
+    virtual void Set_Projection_Transform_With_Z_Bias(const Matrix4x4 & matrix, float znear, float zfar) override;
 
-    virtual void Set_Light(unsigned int index, const LightClass & light);
-    virtual void Set_Ambient(const Vector3 & color);
-    virtual const Vector3 & Get_Ambient() const;
-    virtual void Set_Fog(bool enable, const Vector3 & color, float start, float end);
-    virtual bool Get_Fog_Enable() const;
-    virtual void Set_Light_Environment(LightEnvironmentClass * light_env);
-    virtual LightEnvironmentClass * Get_Light_Environment() const;
+    virtual void Set_Light(unsigned int index, const LightClass & light) override;
+    virtual void Set_Ambient(const Vector3 & color) override;
+    virtual const Vector3 & Get_Ambient() const override;
+    virtual void Set_Fog(bool enable, const Vector3 & color, float start, float end) override;
+    virtual bool Get_Fog_Enable() const override;
+    virtual void Set_Light_Environment(LightEnvironmentClass * light_env) override;
+    virtual LightEnvironmentClass * Get_Light_Environment() const override;
 
     virtual void Draw_Triangles(unsigned short start_index,
                                 unsigned short polygon_count,
                                 unsigned short min_vertex_index,
-                                unsigned short vertex_count);
+                                unsigned short vertex_count) override;
     virtual void Draw_Triangles(unsigned int buffer_type,
                                 unsigned short start_index,
                                 unsigned short polygon_count,
                                 unsigned short min_vertex_index,
-                                unsigned short vertex_count);
+                                unsigned short vertex_count) override;
     virtual void Draw_Strip(unsigned short start_index,
                             unsigned short index_count,
                             unsigned short min_vertex_index,
-                            unsigned short vertex_count);
+                            unsigned short vertex_count) override;
 
     // The shader id is treated as an opaque unsigned long.
-    virtual void Set_Vertex_Shader(unsigned long vertex_shader);
-    virtual void Set_Pixel_Shader(unsigned long pixel_shader);
-    virtual void Set_Vertex_Shader_Constant(int reg, const void * data, int count);
-    virtual void Set_Pixel_Shader_Constant(int reg, const void * data, int count);
+    virtual void Set_Vertex_Shader(unsigned long vertex_shader) override;
+    virtual void Set_Pixel_Shader(unsigned long pixel_shader) override;
+    virtual void Set_Vertex_Shader_Constant(int reg, const void * data, int count) override;
+    virtual void Set_Pixel_Shader_Constant(int reg, const void * data, int count) override;
 
-    virtual TextureClass * Create_Render_Target(int width, int height, WW3DFormat format);
-    virtual void Set_Render_Target_With_Z(TextureClass * texture, ZTextureClass * ztexture);
-    virtual bool Is_Render_To_Texture();
-    virtual void Set_Shadow_Map(int idx, ZTextureClass * ztex);
-    virtual ZTextureClass * Get_Shadow_Map(int idx);
+    virtual TextureClass * Create_Render_Target(int width, int height, WW3DFormat format) override;
+    virtual void Set_Render_Target_With_Z(TextureClass * texture, ZTextureClass * ztexture) override;
+    virtual bool Is_Render_To_Texture() override;
+    virtual void Set_Shadow_Map(int idx, ZTextureClass * ztex) override;
+    virtual ZTextureClass * Get_Shadow_Map(int idx) override;
 };
